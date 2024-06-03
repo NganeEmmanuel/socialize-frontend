2a8b2840fb56c0c05af9823081c3712456953c78
KING1ADB <kingadb181@gmail.com>
2024-06-03 02:06:55 -0700

    Add feature Socialize UI's to ADB

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,12 @@ import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import "./style.scss";
+
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
 
+/** This new */
 function App() {
   const { currentUser } = useContext(AuthContext);
 
